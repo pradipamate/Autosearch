247c1732e5c6e0b06a8ccb5c25f4ade983ea86a0
5377 <pradipamate07@gmail.com>
2020-07-13 17:42:42 +0530

    Updates

---
+++ precache-manifest.9e6a71f33c522f03cd87771d556e8800.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c5ebc8712c28431e0db",
+    "url": "/Autosearch/static/css/main.e9ee6f43.chunk.css"
+  },
+  {
+    "revision": "8c5ebc8712c28431e0db",
+    "url": "/Autosearch/static/js/main.413b3fe8.chunk.js"
+  },
+  {
+    "revision": "804139f49103adf4bf5d",
+    "url": "/Autosearch/static/js/runtime~main.37b3a055.js"
+  },
+  {
+    "revision": "0734c2b556c81bd56db1",
+    "url": "/Autosearch/static/js/2.6d1b3257.chunk.js"
+  },
+  {
+    "revision": "47e48d9355f5b2c886a470c70c3c68c9",
+    "url": "/Autosearch/index.html"
+  }
+];
